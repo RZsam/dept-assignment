0566a05f6dba19407563952d76d23ca101eb6318
sam <=>
2022-08-05 19:37:16 +0430

    feat: install emotion

---
+++ src/theme/index.ts
@@ -0,0 +1,25 @@
+const theme = {
+  colors: {
+    danger: {
+      main: '#FF2929',
+    },
+    text: {
+      white: '#FFF',
+      grey: '#808080',
+      lightGrey: '#A3A3A3',
+      black: '#121212',
+    },
+    background: {
+      white: '#FFF',
+      black: '#121212',
+    },
+    divider: {
+      grey: '#DDDDDD',
+      darkGrey: '#3E3E3E',
+    },
+  },
+
+  spacing: (unit: number = 1) => 8 * unit,
+};
+
+export default theme;

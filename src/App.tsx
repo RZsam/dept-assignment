bd29b815151894ffe8b6b24394b49f8fb30eb130
sam <=>
2022-08-07 20:30:07 +0430

    feat: install react-query and add provider to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import { ThemeProvider } from '@emotion/react';
 import theme from 'theme';
 import Home from 'pages/home';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <Home />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <Home />
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 }
 

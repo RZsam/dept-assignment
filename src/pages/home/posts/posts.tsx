d1d4443d860554903fbf6b02d401a0ccedfaa15b
sam <=>
2022-08-08 16:51:34 +0430

    wip: work on posts

--- src/pages/home/posts/posts.tsx
+++ src/pages/home/posts/posts.tsx
@@ -1,16 +1,29 @@
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react';
 import { useQuery } from '@tanstack/react-query';
-import getData from 'api/get-data';
+import getData, { Post } from 'api/get-data';
 import Text from 'components/text';
 import CardPost from '../card-post';
-import theme from 'theme';
+import ListPost from '../list-post';
+import QoutePost from '../qoute-post';
+import { v4 as uuidv4 } from 'uuid';
 
 const postsCss = {
   container: css({
     display: 'flex',
     flexWrap: 'wrap',
   }),
+};
+const renderPost = (post: Post) => {
+  const id = uuidv4();
+  switch (post.type) {
+    case 'card':
+      return <CardPost post={post} key={id} />;
+    case 'list':
+      return <ListPost post={post} key={id} />;
+    case 'qoute':
+      return <QoutePost post={post} key={id} />;
+  }
 };
 
 const Posts = () => {
@@ -19,14 +32,12 @@ const Posts = () => {
   if (isLoading) {
     return <Text>Loading...</Text>;
   }
+  if (error) {
+    return <Text>oops...! please try again!</Text>;
+  }
 
   return (
-    <div css={postsCss.container}>
-      {data?.map((post, index) => {
-        if (post.type === 'card') return <CardPost post={post} key={index} />;
-        else return 'list';
-      })}
-    </div>
+    <div css={postsCss.container}>{data?.map((post) => renderPost(post))}</div>
   );
 };
 

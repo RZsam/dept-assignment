32e4b0337818769d86db62e1077f8e809e1d3491
sam <=>
2022-08-11 14:30:49 +0430

    feat: make clients responsive

--- src/pages/home/cleints/clients.tsx
+++ src/pages/home/cleints/clients.tsx
@@ -5,6 +5,8 @@ import { getClient } from 'api/get-data';
 import Text from 'components/text';
 import theme from 'theme';
 import Img from 'components/img';
+import mq, { getMaxWidthString } from 'theme/media-queries';
+import useMediaQuery from 'hooks/useMediaQuery';
 
 const clientCss = {
   container: css({
@@ -12,6 +14,10 @@ const clientCss = {
     flexWrap: 'wrap',
     padding: `${theme.spacing(5)}px ${theme.spacing(11.5)}px `,
     backgroundColor: theme.colors.background.black,
+    [mq('xs')]: css({
+      padding: `${theme.spacing(11.5)}px ${theme.spacing(3)}px `,
+      paddingBottom: theme.spacing(4),
+    }),
   }),
   title: css({
     fontSize: 34,
@@ -21,16 +27,32 @@ const clientCss = {
   logoContainer: {
     width: 170,
     height: 90,
+    [mq('xs')]: css({
+      height: 80,
+      width: 115,
+    }),
   },
   clientsLogoContainer: css({
     display: 'flex',
     flexWrap: 'wrap',
     padding: `0 ${theme.spacing(28)}px`,
+    [mq('xs')]: css({
+      padding: 0,
+    }),
+  }),
+  noMargin: css({
+    marginBottom: 0,
   }),
   midLogo: css({ margin: `0 ${theme.spacing(30)}px` }),
   column: css({
     flexBasis: '33%',
     marginBottom: theme.spacing(10),
+    [mq('xs')]: css({
+      flexBasis: '50%',
+      img: {
+        objectFit: 'contain',
+      },
+    }),
   }),
   flexStart: css({ display: 'flex', justifyContent: 'flex-start' }),
   flexCenter: css({ display: 'flex', justifyContent: 'center' }),
@@ -39,6 +61,7 @@ const clientCss = {
 
 const Clients = () => {
   const { isLoading, error, data } = useQuery(['getClients'], getClient);
+  const isXs = useMediaQuery(getMaxWidthString('xs'));
 
   if (isLoading) {
     return <Text>Loading...</Text>;
@@ -56,6 +79,14 @@ const Clients = () => {
         return clientCss.flexEnd;
     }
   };
+  const selectFlexXS = (imgIndex: number) => {
+    switch (imgIndex % 2) {
+      case 0:
+        return clientCss.flexStart;
+      case 1:
+        return clientCss.flexEnd;
+    }
+  };
 
   return (
     <div css={clientCss.container}>
@@ -67,7 +98,13 @@ const Clients = () => {
       </Text>
       <div css={clientCss.clientsLogoContainer}>
         {data?.clients?.map((client, index) => (
-          <div css={[clientCss.column, selectFlex(index)]}>
+          <div
+            css={[
+              clientCss.column,
+              isXs ? selectFlexXS(index) : selectFlex(index),
+              isXs && data?.clients?.length - 3 <= index && clientCss.noMargin,
+            ]}
+          >
             <div css={clientCss.logoContainer} key={index}>
               <Img src={client.logoUrl} alt={client.alt} />
             </div>
